e22b4adbbb6e0a870c89fbf8bbb83065c9725b48
Daniil Novoselov <gudnmail@gmail.com>
2021-12-02 11:56:24 +0500

    feat: add rendering latex

--- tex2png.go
+++ tex2png.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"log"
 	"os"
+	"os/exec"
+	"path"
 	"text/template"
 )
 
@@ -15,11 +17,42 @@ type TemplateArgs struct {
 	Body string
 }
 
+func tex2png(texCode *bytes.Buffer) (string, error) {
+	tmpDir, err := os.MkdirTemp("", "t2p-")
+	if err != nil {
+		return "", err
+	}
+	srcFn := path.Join(tmpDir, "source.tex")
+	os.WriteFile(srcFn, texCode.Bytes(), 0o664)
+	cmd := exec.Command("pdflatex", "-halt-on-error", srcFn)
+	cmd.Dir = tmpDir
+	err = cmd.Run()
+	if err != nil {
+		os.RemoveAll(tmpDir)
+		return "", err
+	}
+	cmd = exec.Command("pdftoppm", path.Join(tmpDir, "source.pdf"), "output", "-png")
+	cmd.Dir = tmpDir
+	err = cmd.Run()
+	if err != nil {
+		os.RemoveAll(tmpDir)
+		return "", err
+	}
+
+	return path.Join(tmpDir, "output-1.png"), nil
+}
+
 func handler(c *fiber.Ctx) error {
 	args := TemplateArgs{string(c.Body())}
 	rendered := &bytes.Buffer{}
 	if err := templates.ExecuteTemplate(rendered, "tex", args); err == nil {
-		return c.SendString(rendered.String())
+		png, err := tex2png(rendered)
+		if err != nil {
+			log.Printf("rendering error: %v", err)
+			return c.SendStatus(500)
+		}
+		defer os.RemoveAll(path.Dir(png))
+		return c.SendFile(png)
 	} else {
 		log.Printf("templating error: %v", err)
 		return c.SendStatus(500)
