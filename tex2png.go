43bb46e092ce3176b072f7bba221d2849189289a
Daniil Novoselov <gudnmail@gmail.com>
2021-12-02 10:42:39 +0500

    feat: add tex template

--- tex2png.go
+++ tex2png.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"log"
 	"os"
+	"text/template"
 )
 
+var templates *template.Template
+
 type TemplateArgs struct {
-  body string
+	Body string
 }
 
 func handler(c *fiber.Ctx) error {
-	args := TemplateArgs {string(c.Body())}
-	return c.SendString(fmt.Sprint(args))
+	args := TemplateArgs{string(c.Body())}
+	rendered := &bytes.Buffer{}
+	if err := templates.ExecuteTemplate(rendered, "tex", args); err == nil {
+		return c.SendString(rendered.String())
+	} else {
+		log.Printf("templating error: %v", err)
+		return c.SendStatus(500)
+	}
 }
 
 func main() {
+	var err error
+	templates, err = template.ParseFiles("templates/tex")
+	if err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 	app.Post("/", handler)
 	app.Listen(fmt.Sprintf(":%s", os.Getenv("T2P_PORT")))
